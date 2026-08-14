ca9e66aff224d53ca8042539f71a66193d0c4fb4
Saumya Devi <saumyasachan587@gmail.com>
2026-08-14 11:03:11 +0530

    Add Telemedicine component with appointment options

--- src/pages/Telemedicine.jsx
+++ src/pages/Telemedicine.jsx
@@ -1,1 +1,14 @@
+import React from "react";
+import "./Telemedicine.css";
+
+export default function Telemedicine() {
+  return (
+    <div className="telemedicine-container">
+      <h2>Telemedicine Consultation</h2>
+      <p>Book an appointment or start a video call with doctors.</p>
+      <button className="btn">Book Appointment</button>
+      <button className="btn video">Start Video Call</button>
+    </div>
+  );
+}
 
